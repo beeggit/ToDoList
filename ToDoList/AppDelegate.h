eb7848df88db90ef3f906fce390f308378870694
beeggit <beeg@MBP-Loaner.local>
2015-02-18 20:54:22 -0700

    Start

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Brad Brown on 2/18/15.
+//  Copyright (c) 2015 Brad Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
